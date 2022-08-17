212268da35f6a1f7f75192d46b30b3bb309eec48
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-17 15:16:43 -0300

    api routes structure

---
+++ routes/solves.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    return
+})
+
+router.get(':id', (req, res) => {
+    return
+})
+
+router.post('/', (req, res) => {
+    return
+})
+
+// patch and delete routes need middlewares ??????
+router.patch('/:id', (req, res) => {
+    return
+})
+
+router.delete('/:id', (req, res) => {
+    return
+})
+
+module.exports = router;
